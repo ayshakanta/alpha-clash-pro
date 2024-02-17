87cb9d1a6a28c0caa52bfb56fcb6d80c7a389fba
ayshakanta <ayshahussain291@gmail.com>
2024-02-18 03:14:29 +0600

    added part1

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,24 @@
+// function play(){
+//     const homeSection = document.getElementById("home-screen")
+//     homeSection.classList.add("hidden")
+
+//     const playGroundSection = document.getElementById("play-ground")
+//     playGroundSection.classList.remove("hidden")
+// }
+
+function continueGame(){
+    const alphabet = getRandomAlphabet();
+    console.log(alphabet)
+
+    const currentAlphabetElement = document.getElementById("current-alphabet")
+    currentAlphabetElement.innerText = alphabet ;
+
+    setBackgroundColorById(alphabet)
+}
+
+function play(){
+    hideElementById("home-screen")
+    showElementById("play-ground");
+
+    continueGame()
+}
---
+++ scripts/utility.js
@@ -0,0 +1,30 @@
+function hideElementById(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.add("hidden")
+}
+
+function showElementById(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.remove("hidden")
+}
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.add("bg-orange-400")
+}
+
+function getRandomAlphabet(){
+    // get or create an alphabet array
+    const alphabetStrings = "abcdefghijklmnopqrstuvwxyz" ;
+    const alphabets = alphabetStrings.split('');
+    // console.log(alphabets)
+
+    // get a random index between 0-25
+    const randomNumber = Math.random()*25 ;
+    const index = Math.round(randomNumber)
+    // console.log(index)
+
+    const alphabet = alphabets[index]
+    // console.log(index, alphabet)
+    return alphabet ;
+}
